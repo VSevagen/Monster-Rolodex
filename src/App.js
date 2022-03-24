634c8bc289e23d50789a25de789d0bdb86b07153
Sevagen Veerasamy <sevagen.veerasamy@hangarww.com>
2022-03-24 14:32:06 +0400

    chore: Optimize App.js
    Signed-off-by: Sevagen Veerasamy <sevagen.veerasamy@hangarww.com>

--- src/App.js
+++ src/App.js
@@ -23,24 +23,31 @@ class App extends Component {
       )
   }
 
-  render() {
+  onSearchChange = (event) => {
+    const lowerCaseString = event.target.value.toLowerCase();
+      this.setState(
+        () => {
+          return { searchString: lowerCaseString };
+        }
+      )
+  }
 
-    const filteredMonsters = this.state.originalMonsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchString);
+  render() {
+    const { searchString, originalMonsters } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = originalMonsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchString);
     });
 
     return(
       <div className="App">
-        <input className="search-box" type="search" placeholder="search monsters" onChange={(event) =>
-          {
-            const lowerCaseString = event.target.value.toLowerCase();
-            this.setState(
-              () => {
-                return { searchString: lowerCaseString };
-              }
-            )
-          }
-        }/>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={ onSearchChange }
+        />
         {filteredMonsters.map((monster) => {
           return(
             <div key={monster.id}>
