6b5cf01c1fd4fec86edb7fa8840eae28cbde4278
Sevagen Veerasamy <sveerasa@PBIO-2859.local>
2022-03-24 13:55:37 +0400

    chore: Initial commit
    Signed-off-by: Sevagen Veerasamy <sveerasa@PBIO-2859.local>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      searchString: "",
+      originalMonsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users =>
+        this.setState(
+          () => {
+            return { originalMonsters: users };
+          }
+        )
+      )
+  }
+
+  render() {
+
+    const filteredMonsters = this.state.originalMonsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchString);
+    });
+
+    return(
+      <div className="App">
+        <input className="search-box" type="search" placeholder="search monsters" onChange={(event) =>
+          {
+            const lowerCaseString = event.target.value.toLowerCase();
+            this.setState(
+              () => {
+                return { searchString: lowerCaseString };
+              }
+            )
+          }
+        }/>
+        {filteredMonsters.map((monster) => {
+          return(
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })}
     </div>
-  );
+    )
+  }
 }
 
 export default App;
